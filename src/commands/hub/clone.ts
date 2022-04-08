5123dda3911144d8ba32158631903f9debf4464e
Florian Krebs <f.krebs@cosnova.com>
2022-04-08 14:59:41 +0200

    feat: remove indexclonestep

--- src/commands/hub/clone.ts
+++ src/commands/hub/clone.ts
@@ -10,7 +10,7 @@ import { ContentCloneStep } from './steps/content-clone-step';
 import { SchemaCloneStep } from './steps/schema-clone-step';
 import { SettingsCloneStep } from './steps/settings-clone-step';
 import { TypeCloneStep } from './steps/type-clone-step';
-import { IndexCloneStep } from './steps/index-clone-step';
+// import { IndexCloneStep } from './steps/index-clone-step';
 import { CloneHubState } from './model/clone-hub-state';
 import { LogErrorLevel } from '../../common/archive/archive-log';
 import { ExtensionCloneStep } from './steps/extension-clone-step';
@@ -46,8 +46,8 @@ export const steps = [
   new ExtensionCloneStep(),
   new SchemaCloneStep(),
   new TypeCloneStep(),
-  new ContentCloneStep(),
-  new IndexCloneStep(),
+  // new IndexCloneStep(),
+  new ContentCloneStep()
 ];
 
 export const builder = (yargs: Argv): void => {
